2bf54fff308f778b62eb35dd5be8d13ba35cbfdb
felipetorreslinux <felipe.torres.oficial@hotmail.com>
2018-07-22 13:40:22 -0300

    app

--- app/src/main/java/com/xticket/Splash.java
+++ app/src/main/java/com/xticket/Splash.java
@@ -1,8 +1,12 @@
 package com.xticket;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.xticket.Views.View_Login;
 
 public class Splash extends Activity {
 
@@ -10,5 +14,15 @@ public class Splash extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, View_Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
     }
 }
--- app/src/main/java/com/xticket/Views/View_Login.java
+++ app/src/main/java/com/xticket/Views/View_Login.java
@@ -1,4 +1,16 @@
 package com.xticket.Views;
 
-public class View_Login {
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.xticket.R;
+
+public class View_Login extends Activity {
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.view_login);
+    }
 }
