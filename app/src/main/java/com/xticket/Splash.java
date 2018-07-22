21707c39923bc09f0276ab890f2b94cbec321c5f
felipetorreslinux <felipe.torres.oficial@hotmail.com>
2018-07-22 13:11:50 -0300

    app

--- app/src/main/java/com/xticket/Splash.java
+++ app/src/main/java/com/xticket/Splash.java
@@ -1,9 +1,10 @@
 package com.xticket;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Splash extends AppCompatActivity {
+public class Splash extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
